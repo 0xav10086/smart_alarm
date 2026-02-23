b949f57d16aa99c81e5ee1eaeade54d2bfd8bae0
0xav10086 <2320334524@qq.com>
2026-02-23 20:45:46 +0800

    perf:优化了项目文件结构，现在sh不再使用，我们将使用kt全面替代原脚本的功能，换言之，从kernelSU -> Lsposed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -22,16 +23,18 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "21"
+        jvmTarget = "17"
     }
 }
 
 dependencies {
-    compileOnly("com.github.libxposed:api:master-SNAPSHOT")
+    compileOnly("io.github.libxposed:api:context-SNAPSHOT")
+//    ksp("io.github.libxposed:api:context-SNAPSHOT")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,18 @@
-pluginManagement {repositories {
-    google()
-    mavenCentral() // 必须有这一行
-    gradlePluginPortal()
-}
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        // 添加阿里云公共仓库镜像，这通常能解决很多依赖下载失败的问题
         maven { url = uri("https://maven.aliyun.com/repository/public") }
         maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
     }
 }
 
